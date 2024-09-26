b4e311849d3808493fc905c21129cd041a2a392a
Luis Vejarano <luis.vejarano@docente.fup.edu.co>
2024-09-26 16:49:08 -0500

    Se crea el proyecto con los ejemplos de los Operadores

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,7 @@
+# Ejemplos utlizando operadores aritmeticos
+numero1 = int(input("Ingrese el numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+
+suma = numero1 + numero2
+print(f"La suma de {numero1} y {numero2} es: {suma}")
+
---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,10 @@
+# Ejemplos de operadores de asignacion
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1 += 3 # var1 = var1 + 3 asignacion con suma
+print(f"Asignacion con suma: {var1}")
